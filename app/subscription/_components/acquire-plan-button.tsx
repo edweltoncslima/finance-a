08936c7b8896d5f95a7328e43932a21cb1ff88ab
edweltoncslima <edweltonchristian.ti@gmail.com>
2024-11-14 21:28:17 -0300

    first commit

---
+++ app/subscription/_components/acquire-plan-button.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { Button } from "@/app/_components/ui/button";
+import { createStripeCheckout } from "../_actions/create-stripe-checkout";
+import { loadStripe } from "@stripe/stripe-js";
+
+const AcquirePlanButton = () => {
+  const handleAcquirePlanClick = async () => {
+    const { sessionId } = await createStripeCheckout();
+    if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
+      throw new Error("Missing Stripe publishable key");
+    }
+    const stripe = await loadStripe(
+      process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+    );
+    if (!stripe) {
+      throw new Error("Stripe is not initialized");
+    }
+    await stripe.redirectToCheckout({ sessionId });
+  };
+  return (
+    <Button
+      className="w-full rounded-full font-bold"
+      onClick={handleAcquirePlanClick}
+    >
+      Adquirir o Plano
+    </Button>
+  );
+};
+export default AcquirePlanButton;
